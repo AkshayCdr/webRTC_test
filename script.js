e95ea09c28bf83e603efcbe91965253b9e4880e2
AkshayCdr <akshaychandra418@gmail.om>
2024-09-27 14:59:51 +0530

    refactored event handling

--- script.js
+++ script.js
@@ -52,184 +52,58 @@ const setRemoteIceCandidateLabel = document.getElementById(
 let peerConnection;
 let dataChannel;
 
-async function init() {
-    try {
-        const localStream = await navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true,
-            video: { width: 300, height: 300 },
-        });
-        localVedioElement.srcObject = localStream;
+const init = async () => {
+    const localStream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+        video: { width: 300, height: 300 },
+    });
+    localVedioElement.srcObject = localStream;
 
-        peerConnection = new RTCPeerConnection(peerConfiguration);
+    peerConnection = new RTCPeerConnection(peerConfiguration);
 
-        localStream.getTracks().forEach((track) => {
-            peerConnection.addTrack(track, localStream);
-        });
+    localStream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, localStream);
+    });
 
-        const channel = peerConnection.createDataChannel("chat");
+    const channel = peerConnection.createDataChannel("chat");
 
+    channel.onopen = (e) => {
+        channel.send("hi me");
+    };
+
+    channel.onmessage = (e) => {
+        console.log(e.data);
+    };
+
+    peerConnection.ondatachannel = (e) => {
+        const channel = e.channel;
         channel.onopen = (e) => {
-            channel.send("hi me");
+            channel.send("hi back");
         };
 
         channel.onmessage = (e) => {
             console.log(e.data);
         };
+    };
 
-        peerConnection.connectionState = (e) => {
-            switch (peerConnection.connectionState) {
-                case "new":
-                case "connecting":
-                    alertStatus("Connecting…");
-                    break;
-                case "connected":
-                    alertStatus("Online");
-                    break;
-                case "disconnected":
-                    alertStatus("Disconnecting…");
-                    break;
-                case "closed":
-                    alertStatus("Offline");
-                    break;
-                case "failed":
-                    alertStatus("Error");
-                    break;
-                default:
-                    alertStatus("Unknown");
-                    break;
-            }
-        };
+    peerConnection.ontrack = handleOnTrack;
 
-        peerConnection.ondatachannel = (e) => {
-            const channel = e.channel;
-            channel.onopen = (e) => {
-                channel.send("hi back");
-            };
+    peerConnection.onicecandidate = handleOnIcecandidate;
 
-            channel.onmessage = (e) => {
-                console.log(e.data);
-            };
-        };
+    btnCreateOffer.addEventListener("click", createOffer);
 
-        peerConnection.ontrack = (e) => {
-            const [data] = e.streams;
-            remoteVedioElement.srcObject = data;
-        };
+    btnSetRemoteOffer.addEventListener("click", handleSetRemoteOffer);
 
-        peerConnection.onicecandidate = (e) => {
-            console.log("ICE candidate found");
-            console.log(e);
-            if (e.candidate) {
-                localIceCandidate.value += JSON.stringify(e.candidate) + "\n";
-            }
-        };
+    btnSetRemoteIceCandidate.addEventListener(
+        "click",
+        handleSetRemoteIceCandidate
+    );
 
-        btnCreateOffer.addEventListener("click", createOffer);
+    btnSetRemoteAnswer.addEventListener("click", handleSetRemoteAns);
+};
 
-        btnSetRemoteOffer.addEventListener("click", () => {
-            console.log("setting remote ice candidate");
-
-            const offer = remoteOffer.value;
-
-            if (!offer) return;
-
-            peerConnection.setRemoteDescription(JSON.parse(offer));
-
-            peerConnection
-                .createAnswer()
-                .then((answer) => {
-                    console.log("answer is ");
-                    console.log(answer);
-                    peerConnection.setLocalDescription(answer);
-                    answerText.value = JSON.stringify(answer) + "\n";
-                })
-                .then(() => {
-                    //dom man
-
-                    btnSetRemoteOffer.disabled = true;
-
-                    btnSetRemoteOffer.classList.add("blur");
-
-                    btnCreateOffer.disabled = true;
-
-                    btnCreateOffer.classList.add("blur");
-
-                    offerText.disabled = true;
-
-                    offerText.classList.add("blur");
-
-                    btnSetRemoteAnswer.disabled = true;
-
-                    btnSetRemoteAnswer.classList.add("blur");
-
-                    remoteAnswer.disabled = true;
-
-                    remoteAnswer.classList.add("blur");
-
-                    createOfferLabel.classList.add("blur", "overline");
-
-                    setRemoteAnsLabel.classList.add("blur", "overline");
-
-                    setRemoteOfferLabel.classList.add("blur");
-
-                    setRemoteOfferCheck.checked = true;
-                });
-        });
-
-        btnSetRemoteIceCandidate.addEventListener("click", async () => {
-            const iceCandidates = remoteICeCandidate.value.split("\n");
-
-            if (!iceCandidates.length) return;
-
-            console.log("ice candidate is ");
-            console.log(iceCandidates);
-
-            iceCandidates.forEach((candidate) => {
-                if (candidate) {
-                    peerConnection
-                        .addIceCandidate(
-                            new RTCIceCandidate(JSON.parse(candidate))
-                        )
-                        .then((ans) => console.log("added ice ", ans))
-                        .catch((err) => console.error(err));
-                }
-            });
-
-            //dom man
-
-            btnSetRemoteIceCandidate.disabled = true;
-
-            btnSetRemoteIceCandidate.classList.add("blur");
-
-            setRemoteIceCandidateCheck.checked = true;
-
-            setRemoteIceCandidateLabel.classList.add("blur");
-        });
-    } catch (error) {
-        console.error("Error initializing:", error);
-    }
-
-    btnSetRemoteAnswer.addEventListener("click", () => {
-        const answer = remoteAnswer.value;
-
-        if (!answer) return;
-
-        peerConnection.setRemoteDescription(JSON.parse(answer));
-        console.log("remote answer set");
-
-        //dom man
-        btnSetRemoteAnswer.disabled = true;
-
-        btnSetRemoteAnswer.classList.add("blur");
-
-        setRemoteAnsCheck.checked = true;
-
-        setRemoteAnsLabel.classList.add("blur");
-    });
-}
-
-async function createOffer() {
+const createOffer = async () => {
     try {
         const offer = await peerConnection.createOffer();
 
@@ -261,10 +135,115 @@ async function createOffer() {
     } catch (error) {
         console.error("Error creating offer:", error);
     }
-}
+};
 
 init();
 
-function alertStatus(status) {
-    alert(status);
-}
+const handleSetRemoteAns = () => {
+    const answer = remoteAnswer.value;
+
+    if (!answer) return;
+
+    peerConnection.setRemoteDescription(JSON.parse(answer));
+    console.log("remote answer set");
+
+    //dom man
+    btnSetRemoteAnswer.disabled = true;
+
+    btnSetRemoteAnswer.classList.add("blur");
+
+    setRemoteAnsCheck.checked = true;
+
+    setRemoteAnsLabel.classList.add("blur");
+};
+
+const handleSetRemoteIceCandidate = async () => {
+    const iceCandidates = remoteICeCandidate.value.split("\n");
+
+    if (!iceCandidates.length) return;
+
+    console.log("ice candidate is ");
+    console.log(iceCandidates);
+
+    iceCandidates.forEach((candidate) => {
+        if (candidate) {
+            peerConnection
+                .addIceCandidate(new RTCIceCandidate(JSON.parse(candidate)))
+                .then((ans) => console.log("added ice ", ans))
+                .catch((err) => console.error(err));
+        }
+    });
+
+    //dom man
+
+    btnSetRemoteIceCandidate.disabled = true;
+
+    btnSetRemoteIceCandidate.classList.add("blur");
+
+    setRemoteIceCandidateCheck.checked = true;
+
+    setRemoteIceCandidateLabel.classList.add("blur");
+};
+
+const handleSetRemoteOffer = () => {
+    console.log("setting remote ice candidate");
+
+    const offer = remoteOffer.value;
+
+    if (!offer) return;
+
+    peerConnection.setRemoteDescription(JSON.parse(offer));
+
+    peerConnection
+        .createAnswer()
+        .then((answer) => {
+            console.log("answer is ");
+            console.log(answer);
+            peerConnection.setLocalDescription(answer);
+            answerText.value = JSON.stringify(answer) + "\n";
+        })
+        .then(() => {
+            //dom man
+
+            btnSetRemoteOffer.disabled = true;
+
+            btnSetRemoteOffer.classList.add("blur");
+
+            btnCreateOffer.disabled = true;
+
+            btnCreateOffer.classList.add("blur");
+
+            offerText.disabled = true;
+
+            offerText.classList.add("blur");
+
+            btnSetRemoteAnswer.disabled = true;
+
+            btnSetRemoteAnswer.classList.add("blur");
+
+            remoteAnswer.disabled = true;
+
+            remoteAnswer.classList.add("blur");
+
+            createOfferLabel.classList.add("blur", "overline");
+
+            setRemoteAnsLabel.classList.add("blur", "overline");
+
+            setRemoteOfferLabel.classList.add("blur");
+
+            setRemoteOfferCheck.checked = true;
+        });
+};
+
+const handleOnIcecandidate = (e) => {
+    console.log("ICE candidate found");
+    console.log(e);
+    if (e.candidate) {
+        localIceCandidate.value += JSON.stringify(e.candidate) + "\n";
+    }
+};
+
+const handleOnTrack = (e) => {
+    const [data] = e.streams;
+    remoteVedioElement.srcObject = data;
+};
